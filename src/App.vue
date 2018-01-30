4c77a614e3844404cc59d5bed72bc5bb486bb18b
genepan <gingerpan2641@gmail.com>
2018-01-30 21:51:32 +0800

    update example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <h1>Vuex-twzipcode</h1>
+    <p>modify from 
+      <a href="https://www.npmjs.com/package/vue2-twzipcode">vue2-twzipcode</a> 
+      by knovour <br>and <br>
+      <a href="https://github.com/CasperLaiTW/vue-twzipcode">vue-twzipcode</a> 
+      by CasperLaiTW
+      </p>
+
+    <h3>example</h3>
     <address-input></address-input>
   </div>
 </template>
