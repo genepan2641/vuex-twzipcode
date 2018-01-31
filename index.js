88448cb83e053ad35c5015ca39562742d8b0f693
genepan <gingerpan2641@gmail.com>
2018-01-31 11:24:08 +0800

    1. rename module name -> twzipcode

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import module from './src/store/modules/address_input';
-import addressInput from './src/components/address_input.vue';
 import twzipcode from './src/components/twzipcode.vue';
 
 
@@ -7,10 +6,9 @@ import twzipcode from './src/components/twzipcode.vue';
 function install(Vue, options = {}) {
     if (!options.store) console.log('Please provide a store!!')
 
-    Vue.component('address-input', addressInput);
     Vue.component('twzipcode', twzipcode);
 
-    options.store.registerModule('addressInput', module)
+    options.store.registerModule('twzipcode', module)
 }
 
 export default {
